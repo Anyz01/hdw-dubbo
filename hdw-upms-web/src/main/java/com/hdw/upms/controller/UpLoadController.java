0e1483ebc9c65d09674cf8125052d1c65c1df32d
tumao2 <tuminglong@126.com>
2018-06-06 22:25:36 +0800

    集成fastdfs

--- hdw-upms-web/src/main/java/com/hdw/upms/controller/UpLoadController.java
+++ hdw-upms-web/src/main/java/com/hdw/upms/controller/UpLoadController.java
@@ -2,9 +2,16 @@ package com.hdw.upms.controller;
 
 import com.google.common.base.Charsets;
 import com.hdw.common.base.BaseController;
+import com.hdw.common.result.Result;
 import com.hdw.common.util.DateUtil;
 import com.hdw.common.util.URLUtils;
+import com.luhuiguo.fastdfs.domain.StorePath;
+import com.luhuiguo.fastdfs.exception.FdfsUnsupportStorePathException;
+import com.luhuiguo.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -17,10 +24,8 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 /**
  * 
@@ -49,6 +54,15 @@ public abstract class UpLoadController extends BaseController {
 	 */
 	@Value("${hdw.file-upload.server}")
 	private String fileUploadServer;
+
+	/**
+	 * FastDFS文件上传服务器名称
+	 */
+	@Value("${fdfs.file-upload.server}")
+	private String fdfsfileUploadServer;
+
+	@Autowired
+	private FastFileStorageClient fastFileStorageClient;
 
 	/**
 	 * 下载文件
@@ -185,6 +199,200 @@ public abstract class UpLoadController extends BaseController {
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			throw new RuntimeException("保存文件异常，请联系管理员");
+		}
+	}
+
+	/**
+	 * 上传文件到FastDFS
+	 * 
+	 * @param file
+	 * @return
+	 * @throws RuntimeException
+	 */
+	public Map<String, Object> uploadToFastDFS(MultipartFile file) throws RuntimeException {
+		try {
+			String fileName = file.getOriginalFilename();
+			StorePath storePath = fastFileStorageClient.uploadFile(IOUtils.toByteArray(file.getInputStream()),
+					FilenameUtils.getExtension(file.getOriginalFilename()));
+			// StorePath storePath =
+			// fastFileStorageClient.uploadFile(IOUtils.toByteArray(new
+			// FileInputStream(file)),FilenameUtils.getExtension(file.getName()));
+			System.out.println("上传文件路径：" + storePath.getFullPath());
+			logger.info("文件分组：" + storePath.getGroup() + "上传文件路径：" + storePath.getFullPath());
+			String path = fdfsfileUploadServer + "/" + storePath.getFullPath() + "?attname=" + fileName;
+			Map<String, Object> par = new HashMap<>();
+			par.put(fileName, path);
+			return par;
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.getMessage());
+			throw new RuntimeException("保存文件异常，请联系管理员");
+		}
+	}
+
+	/**
+	 * 多文件长传到FastDFS
+	 * 
+	 * @param files
+	 * @param dir
+	 * @return
+	 * @throws RuntimeException
+	 */
+	public Map<String, Object> uploadsToFastDFS(MultipartFile[] files, String dir) throws RuntimeException {
+		Map<String, Object> par = new HashMap<>();
+		try {
+			// 判断file数组不能为空并且长度大于0
+			if (files != null && files.length > 0) {
+				// 循环获取file数组中得文件
+				for (int i = 0; i < files.length; i++) {
+					MultipartFile file = files[i];
+					String fileName = file.getOriginalFilename();
+					StorePath storePath = fastFileStorageClient.uploadFile(IOUtils.toByteArray(file.getInputStream()),
+							FilenameUtils.getExtension(file.getOriginalFilename()));
+					// StorePath storePath =
+					// fastFileStorageClient.uploadFile(IOUtils.toByteArray(new
+					// FileInputStream(file)),FilenameUtils.getExtension(file.getName()));
+					System.out.println("上传文件路径：" + storePath.getFullPath());
+					logger.info("文件分组：" + storePath.getGroup() + "上传文件路径：" + storePath.getFullPath());
+					String path = fdfsfileUploadServer + "/" + storePath.getFullPath() + "?attname=" + fileName;
+					par.put(fileName, path);
+				}
+			}
+			return par;
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.getMessage());
+			throw new RuntimeException("保存文件异常，请联系管理员");
+		}
+	}
+
+	/**
+	 * 从FastDFS上下载文件
+	 * 
+	 * @param fileUrl
+	 *            源文件路径
+	 * @param filePath
+	 *            保存路径（不需要带文件名）
+	 * @return
+	 */
+	public Result downloadFileFromFastDFS(String fileUrl, String filePath) throws RuntimeException {
+		try {
+			String temp = fileUrl.substring(fileUrl.indexOf("group"), fileUrl.indexOf("?"));
+			String group = temp.substring(0, temp.indexOf("/"));
+			String path = temp.substring(temp.indexOf("/") + 1);
+			String fileName = fileUrl.substring(fileUrl.indexOf("=") + 1);
+			byte[] bfile = fastFileStorageClient.downloadFile(group, path);
+			getFile(bfile, filePath, fileName);
+			Result r = new Result();
+			r.setMsg("文件下载成功");
+			r.setSuccess(true);
+			r.setCode(1);
+			return r;
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.getMessage());
+			throw new RuntimeException("保存文件异常，请联系管理员");
+		}
+
+	}
+
+	/**
+	 * 从FastDFS上删除文件
+	 * 
+	 * @param fileUrl
+	 *            源文件路径
+	 */
+	public Result deleteFileFromFastDFS(String fileUrl) throws RuntimeException {
+		if (StringUtils.isEmpty(fileUrl)) {
+			Result r = new Result();
+			r.setMsg("文件删除失败");
+			r.setSuccess(false);
+			r.setCode(-1);
+			return r;
+		}
+		try {
+			String temp = fileUrl.substring(fileUrl.indexOf("group"), fileUrl.indexOf("?"));
+			String group = temp.substring(0, temp.indexOf("/"));
+			String path = temp.substring(temp.indexOf("/") + 1);
+			fastFileStorageClient.deleteFile(group, path);
+			Result r = new Result();
+			r.setMsg("文件删除成功");
+			r.setSuccess(true);
+			r.setCode(1);
+			return r;
+		} catch (FdfsUnsupportStorePathException e) {
+			e.printStackTrace();
+			logger.error(e.getMessage());
+			throw new RuntimeException("删除文件异常，请联系管理员");
+		}
+	}
+
+	/**
+	 * 获得指定文件的byte数组
+	 * 
+	 * @param filePath
+	 * @return
+	 */
+	@SuppressWarnings("unused")
+	private byte[] getBytes(String filePath) {
+		byte[] buffer = null;
+		try {
+			File file = new File(filePath);
+			FileInputStream fis = new FileInputStream(file);
+			ByteArrayOutputStream bos = new ByteArrayOutputStream(1000);
+			byte[] b = new byte[1000];
+			int n;
+			while ((n = fis.read(b)) != -1) {
+				bos.write(b, 0, n);
+			}
+			fis.close();
+			bos.close();
+			buffer = bos.toByteArray();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return buffer;
+	}
+
+	/**
+	 * 根据byte数组，生成文件
+	 * 
+	 * @param bfile
+	 * @param filePath
+	 * @param fileName
+	 */
+	private void getFile(byte[] bfile, String filePath, String fileName) {
+		BufferedOutputStream bos = null;
+		FileOutputStream fos = null;
+		File file = null;
+		try {
+			File dir = new File(filePath);
+			if (!dir.exists() && dir.isDirectory()) {// 判断文件目录是否存在
+				dir.mkdirs();
+			}
+			file = new File(filePath + "\\" + fileName);
+			fos = new FileOutputStream(file);
+			bos = new BufferedOutputStream(fos);
+			bos.write(bfile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (bos != null) {
+				try {
+					bos.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
 		}
 	}
 
